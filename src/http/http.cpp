4b27af37449f8feb13a6e6799902929b23761b8f
Uladzimir Kireyeu <ukireyeu@c4r1s5.42warsaw.pl>
2025-02-05 16:39:26 +0100

    Updates on Request class

--- src/http/http.cpp
+++ src/http/http.cpp
@@ -1,5 +1,5 @@
 #include "http.hpp"
-#include "../utils/utils.hpp"
+#include <cstddef>
 #include <cstring>
 #include <stdexcept>
 #include <unistd.h>
@@ -12,14 +12,15 @@ http::webbuf::webbuf(int size) {
   this->end = 0;
 }
 http::webbuf::~webbuf() { delete this->buffer; }
+
 char *http::webbuf::getBuffer() { return this->buffer; }
-int http::webbuf::getEnd() { return this->end; }
+size_t http::webbuf::getEnd() { return this->end; }
 void http::webbuf::setEnd(int end) {
   this->buffer[end] = '\0';
   this->end = end;
   setFull();
 }
-int http::webbuf::getSize() { return size; }
+size_t http::webbuf::getSize() { return size; }
 bool http::webbuf::isFull() { return full; }
 void http::webbuf::setFull() { full = true; }
 void http::webbuf::readBuf(int fd) {
@@ -37,33 +38,54 @@ void http::webbuf::readBuf(int fd) {
 
 // Request
 http::Request::Request(srvparams const &params)
-    : buffers_status(http::NOTHING_DONE), request_line_len(0),
-      header_buffer(params.client_header_buffer_size) {}
+    : methodParsed(false), uriParsed(false), httpversParsed(false), currbuff(0),
+      cursor(0), endseq_cnt(0), status(http::NOTHING_DONE), line_len(0) {
+  header_buffers.push_back(webbuf(params.client_header_buffer_size));
+}
 http::Request::~Request() {}
 
 void http::Request::handleData(int fd, srvparams const &params) {
-  // TODO: handling parsing
-  // TODO: body handling if it fits into header buffer
-  if (!header_buffer.isFull()) {
-    header_buffer.readBuf(fd);
+  // if status = headers_done
+  // parse and buffer the body
+  if (!header_buffers[0].isFull()) {
+    header_buffers[0].readBuf(fd);
     // TODO: we will have to check for this line in all of the buffers.
-    if (utils::seqPresent((char *)"\r\n\r\n", header_buffer.getBuffer())) {
-      header_buffer.setFull();
-      buffers_status = HEADERS_DONE;
-      // TODO: ADD \0 THAT it's the end of the buffer, the rest to the body
-      // class
-    }
-    if (buffers_status == NOTHING_DONE) {
-      char *nloc = strchr(header_buffer.getBuffer() + line_len, '\n');
-      if (nloc != NULL) {
-        buffers_status = REQUEST_LINE_DONE;
-        line_len = nloc - header_buffer.getBuffer();
-      } else {
-        line_len = header_buffer.getEnd();
+  } else {
+    /**
+     * Check if the large buffers for headers are created.
+     * If not, initialize them
+     */
+    if (header_buffers.size() == 1) {
+      for (int i = 0; i < params.large_client_header_buffers_number; ++i) {
+        header_buffers.push_back(
+            webbuf(params.large_client_header_buffers_size));
       }
-      if (line_len > params.large_client_header_buffers_size)
-        throw http::HttpError(http::URITooLong);
+      ++currbuff;
     }
+    header_buffers[currbuff].readBuf(fd);
+  }
+  // Parsing
+  while (cursor < header_buffers[currbuff].getEnd()) {
+    char *headers_endseq = (char *)"\r\n\r\n";
+    if (header_buffers[currbuff].getBuffer()[cursor] ==
+        headers_endseq[endseq_cnt])
+      ++endseq_cnt;
+    ++cursor;
+  }
+  // /Parsing
+
+  /**
+   * Check for buffers, if the current is full
+   * then go to the next one. If all of the buffers are full,
+   * and the headers processing is not over yet - BadRequest 400
+   */
+  if (header_buffers[currbuff].isFull()) {
+    ++currbuff;
+    cursor = 0;
+    if (currbuff == (size_t)params.large_client_header_buffers_number &&
+        status != HEADERS_DONE)
+      throw http::HttpError("Request line and headers are too long",
+                            http::BadRequest);
   }
 }
 // /Request
--- src/http/http.hpp
+++ src/http/http.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "../server/server.hpp"
+#include <cstddef>
 #include <cstring>
 #include <exception>
-#include <string>
+#include <utility>
+#include <vector>
 
 namespace http {
 enum RequestStatus {
@@ -15,104 +17,108 @@ enum RequestStatus {
 };
 
 // Statuses
-static const char *Continue = "100";
-static const char *SwitchingProtocols = "101";
-static const char *Processing = "102";
-static const char *EarlyHints = "103";
+static const int Continue = 100;
+static const int SwitchingProtocols = 101;
+static const int Processing = 102;
+static const int EarlyHints = 103;
 
 // Successful responses
-static const char *OK = "200";
-static const char *Created = "201";
-static const char *Accepted = "202";
-static const char *NonAuthoritativeInformation = "203";
-static const char *NoContent = "204";
-static const char *ResetContent = "205";
-static const char *PartialContent = "206";
-static const char *MultiStatus = "207";
-static const char *AlreadyReported = "208";
-static const char *IMUsed = "226";
+static const int OK = 200;
+static const int Created = 201;
+static const int Accepted = 202;
+static const int NonAuthoritativeInformation = 203;
+static const int NoContent = 204;
+static const int ResetContent = 205;
+static const int PartialContent = 206;
+static const int MultiStatus = 207;
+static const int AlreadyReported = 208;
+static const int IMUsed = 226;
 
 // Redirection messages
-static const char *MultipleChoices = "300";
-static const char *MovedPermanently = "301";
-static const char *Found = "302";
-static const char *SeeOther = "303";
-static const char *NotModified = "304";
-static const char *UseProxy = "305"; // Deprecated
-static const char *Unused = "306";   // Reserved
-static const char *TemporaryRedirect = "307";
-static const char *PermanentRedirect = "308";
+static const int MultipleChoices = 300;
+static const int MovedPermanently = 301;
+static const int Found = 302;
+static const int SeeOther = 303;
+static const int NotModified = 304;
+static const int UseProxy = 305; // Deprecated
+static const int Unused = 306;   // Reserved
+static const int TemporaryRedirect = 307;
+static const int PermanentRedirect = 308;
 
 // Client error responses
-static const char *BadRequest = "400";
-static const char *Unauthorized = "401";
-static const char *PaymentRequired = "402";
-static const char *Forbidden = "403";
-static const char *NotFound = "404";
-static const char *MethodNotAllowed = "405";
-static const char *NotAcceptable = "406";
-static const char *ProxyAuthenticationRequired = "407";
-static const char *RequestTimeout = "408";
-static const char *Conflict = "409";
-static const char *Gone = "410";
-static const char *LengthRequired = "411";
-static const char *PreconditionFailed = "412";
-static const char *ContentTooLarge = "413";
-static const char *URITooLong = "414";
-static const char *UnsupportedMediaType = "415";
-static const char *RangeNotSatisfiable = "416";
-static const char *ExpectationFailed = "417";
-static const char *ImATeapot = "418";
-static const char *MisdirectedRequest = "421";
-static const char *UnprocessableContent = "422";
-static const char *Locked = "423";
-static const char *FailedDependency = "424";
-static const char *TooEarly = "425";
-static const char *UpgradeRequired = "426";
-static const char *PreconditionRequired = "428";
-static const char *TooManyRequests = "429";
-static const char *RequestHeaderFieldsTooLarge = "431";
-static const char *UnavailableForLegalReasons = "451";
+static const int BadRequest = 400;
+static const int Unauthorized = 401;
+static const int PaymentRequired = 402;
+static const int Forbidden = 403;
+static const int NotFound = 404;
+static const int MethodNotAllowed = 405;
+static const int NotAcceptable = 406;
+static const int ProxyAuthenticationRequired = 407;
+static const int RequestTimeout = 408;
+static const int Conflict = 409;
+static const int Gone = 410;
+static const int LengthRequired = 411;
+static const int PreconditionFailed = 412;
+static const int ContentTooLarge = 413;
+static const int URITooLong = 414;
+static const int UnsupportedMediaType = 415;
+static const int RangeNotSatisfiable = 416;
+static const int ExpectationFailed = 417;
+static const int ImATeapot = 418;
+static const int MisdirectedRequest = 421;
+static const int UnprocessableContent = 422;
+static const int Locked = 423;
+static const int FailedDependency = 424;
+static const int TooEarly = 425;
+static const int UpgradeRequired = 426;
+static const int PreconditionRequired = 428;
+static const int TooManyRequests = 429;
+static const int RequestHeaderFieldsTooLarge = 431;
+static const int UnavailableForLegalReasons = 451;
 
 // Server error responses
-static const char *InternalServerError = "500";
-static const char *NotImplemented = "501";
-static const char *BadGateway = "502";
-static const char *ServiceUnavailable = "503";
-static const char *GatewayTimeout = "504";
-static const char *HTTPVersionNotSupported = "505";
-static const char *VariantAlsoNegotiates = "506";
-static const char *InsufficientStorage = "507";
-static const char *LoopDetected = "508";
-static const char *NotExtended = "510";
-static const char *NetworkAuthenticationRequired = "511";
+static const int InternalServerError = 500;
+static const int NotImplemented = 501;
+static const int BadGateway = 502;
+static const int ServiceUnavailable = 503;
+static const int GatewayTimeout = 504;
+static const int HTTPVersionNotSupported = 505;
+static const int VariantAlsoNegotiates = 506;
+static const int InsufficientStorage = 507;
+static const int LoopDetected = 508;
+static const int NotExtended = 510;
+static const int NetworkAuthenticationRequired = 511;
 // /Statuses
 
 class HttpError : public std::exception {
 private:
   char message[256]; // Fixed-size buffer to store the error message
+  int statusCode;
 
 public:
-  HttpError(const char *msg) {
+  HttpError(const char *msg, int statusCode) {
     std::strncpy(message, msg, sizeof(message) - 1);
     message[sizeof(message) - 1] = '\0';
+    this->statusCode = statusCode;
   }
   const char *what() const throw() { return message; }
+  int getStatus() const { return statusCode; }
 };
+
 // Buffer
 class webbuf {
 private:
   char *buffer;
-  int size;
-  int end;
+  size_t size;
+  size_t end;
   bool full;
 
 public:
   void setFull();
   bool isFull();
   void readBuf(int fd);
-  int getSize();
-  int getEnd();
+  size_t getSize();
+  size_t getEnd();
   void setEnd(int end);
   char *getBuffer();
   webbuf(int size);
@@ -123,18 +129,26 @@ public:
 // Request
 class Request {
 private:
-  std::string method;
-  std::string uri;
-  std::string http_version;
+  char *method;
+  bool methodParsed;
+  char *uri;
+  bool uriParsed;
+  char *httpvers;
+  bool httpversParsed;
+  // clang-format off
+  std::vector<std::pair<char *, char *> > headers;
+  // clang-format on
+  std::vector<webbuf> header_buffers;
+  size_t currbuff;
+  size_t cursor;
+  int endseq_cnt;
   // Utils
-  RequestStatus parsing_status;
+  RequestStatus status;
   // /Utils
 
 public:
   void handleData(int fd, srvparams const &params);
-  RequestStatus buffers_status;
   int line_len;
-  webbuf header_buffer;
   Request(srvparams const &params);
   Request &operator=(Request const &req);
   // TODO: Delete buffers
